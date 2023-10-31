74609e0349c9624ac93c5c1c1fb8bbb4c1cff4db
fisher <fisheralexandr@gmail.com>
2023-10-31 15:03:56 +0300

    remove AuthProviderImpl

--- src/main/java/com/fisher/securityapp/config/SpringSecurityConfig.java
+++ src/main/java/com/fisher/securityapp/config/SpringSecurityConfig.java
@@ -1,19 +1,27 @@
 package com.fisher.securityapp.config;
 
-import com.fisher.securityapp.security.AuthProviderImpl;
+import com.fisher.securityapp.service.PersonDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
