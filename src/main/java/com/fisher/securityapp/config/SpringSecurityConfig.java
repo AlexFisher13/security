fbe4b6d270fcf16a16872e4dd7c24da2a0c7056e
fisher <fisheralexandr@gmail.com>
2023-11-01 17:36:47 +0300

    authorization configure

--- src/main/java/com/fisher/securityapp/config/SpringSecurityConfig.java
+++ src/main/java/com/fisher/securityapp/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import com.fisher.securityapp.service.PersonDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -16,8 +17,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     private final PersonDetailsService personDetailsService;
 
     @Override
+    // Конфигурируем Аутентификацию
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
+    }
+
+    @Override
+    // Конфигурируем работу с http запросами, а именно
+    // авторизацию (т.е. какие страницы доступны)
+    // страницы входа, страницы после успеха и неудачи
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests() // настраиваем авторизацию (т.е. доступы)
+                .antMatchers("/auth/login", "/error").permitAll() // эти страницы доступны всем
+                .anyRequest().authenticated() // для других запросов юзер должен аутентифицироваться
+                .and()
+                .formLogin().loginPage("/auth/login") // на какую страницу вести неавторизованных
+                .loginProcessingUrl("/process_login") // адрес того куда отправлять данные с формы
+                .defaultSuccessUrl("/hello", true) // страница после удачной аутентификации
+                .failureUrl("/auth/login?error"); // страница после не удачной аутентификации
     }
 
     @Bean
